e2ecc94b2e95cb33935b8e23403b961e25946ff0
Iryna Tsupryk <iranaumovich0@gmail.com>
2023-06-21 13:54:01 +0200

    Сменить код ошибки

--- controllers/users.js
+++ controllers/users.js
@@ -129,7 +129,7 @@ module.exports.login = (req, res, next) => {
   User.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new InvalidUserDataError('Введен некорректный email'));
+        return Promise.reject(new InvalidCredentialsError('Введен некорректный email'));
       }
 
       return bcrypt.compare(password, user.password)

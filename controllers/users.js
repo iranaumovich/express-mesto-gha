6693e142d0cf04a01b00545639668a6d6cca4a56
Iryna Tsupryk <iranaumovich0@gmail.com>
2023-06-21 14:05:33 +0200

    Отправить куки

--- controllers/users.js
+++ controllers/users.js
@@ -144,11 +144,12 @@ module.exports.login = (req, res, next) => {
             'token-secret-key',
             { expiresIn: '7d' },
           );
-          return res
+
+          res
             .cookie('jwt', token, {
               maxAge: 3600000 * 24 * 7,
               httpOnly: true,
-            });
+            }).send('Authorized');
         });
     })
     .catch(next);
